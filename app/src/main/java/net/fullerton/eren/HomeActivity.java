23622d9783ad6b57bad326c7a159c82f68a636d1
marchristiancont <marchristiancontreras@gmail.com>
2021-02-04 16:22:31 -0800

    Webview minor setup in HomeActivity

--- app/src/main/java/net/fullerton/eren/HomeActivity.java
+++ app/src/main/java/net/fullerton/eren/HomeActivity.java
@@ -1,15 +1,23 @@
 package net.fullerton.eren;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
+
+import net.fullerton.eren.handlers.JSFunc;
 
 public class HomeActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private WebView mWebView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,6 +47,24 @@ public class HomeActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mWebView = (WebView) findViewById(R.id.webview);
+        //mWebView.setVisibility(View.GONE);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.loadUrl("https://my.fullerton.edu");
+
+        WebViewClient webViewClient = new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                System.out.println("URL PUSHED: " + request.getUrl());
+                return super.shouldOverrideUrlLoading(view, request);
+            }
     }
 
 }
