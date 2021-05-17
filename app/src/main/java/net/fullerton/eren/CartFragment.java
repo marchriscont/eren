b3e93863a056000beb6aa7522131817467a4b002
Raul Cruz <raulcollege2@yahoo.com>
2021-05-17 02:26:13 -0700

    Test push

--- app/src/main/java/net/fullerton/eren/CartFragment.java
+++ app/src/main/java/net/fullerton/eren/CartFragment.java
@@ -30,16 +30,12 @@ public class CartFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
      * @return A new instance of fragment CartFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static CartFragment newInstance(String param1, String param2) {
+    public static CartFragment newInstance() {
         CartFragment fragment = new CartFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
